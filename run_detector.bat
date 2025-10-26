66efdd6222439665381e10574238a1f90b837d34
Scott Norton <scott.norton12@gmail.com>
2025-10-26 17:16:31 -0400

    ignore .bat files

--- run_detector.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Sports Broadcast Detector Launcher
-REM Double-click this file to start the detector
-REM You can edit this file to specify a different sport (e.g., --sport nba)
-
-cd /d "%~dp0"
-python detector.py
-pause
-
